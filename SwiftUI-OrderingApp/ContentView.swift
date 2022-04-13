c522c7eea12e0f2a26f48e2250e768f95cd82ab3
Ethan Halprin <ethan.halprin@gmail.com>
2022-04-13 10:05:55 +0300

    Initial Commit

---
+++ SwiftUI-OrderingApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-OrderingApp
+//
+//  Created by Ethan on 13/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-OrderingApp/SwiftUI_OrderingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_OrderingAppApp.swift
+//  SwiftUI-OrderingApp
+//
+//  Created by Ethan on 13/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_OrderingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
